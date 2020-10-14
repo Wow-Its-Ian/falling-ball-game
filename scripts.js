17e111a829eb8085921027e89877304d73258b55
Ian Walls <wallsianh@gmail.com>
2020-10-14 17:13:12 -0400

    ball movement and bounds added

--- scripts.js
+++ scripts.js
@@ -1,1 +1,34 @@
- 
+ const character = document.getElementById("character");
+ let interval;
+ let both = 0;
+
+ function moveLeft() {
+     const left = parseInt(window.getComputedStyle(character).getPropertyValue("left"));
+     if(left > 0) {
+        character.style.left = left - 2 + "px";
+    }
+ }
+
+ function moveRight() {
+    const left = parseInt(window.getComputedStyle(character).getPropertyValue("left"));
+    if(left < 380) {
+        character.style.left = left + 2 + "px";
+    }
+}
+
+document.addEventListener("keydown", event => {
+    if(both === 0) {
+        both ++;
+        if(event.key === "ArrowLeft") {
+            interval = setInterval(moveLeft, 1);
+        }
+        if(event.key === "ArrowRight") {
+            interval = setInterval(moveRight, 1);
+        }
+    }
+})
+
+document.addEventListener("keyup", event => {
+    clearInterval(interval);
+    both = 0;
+})
